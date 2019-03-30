5f0c48dd6594b41a883e2a16a4c1860694f1e3a3
Carden Z <ephemere@protonmail.com>
2019-03-30 01:10:36 +0000

    Automatically backed up by Learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,8 +1,9 @@
 class Fish
   attr_reader :name
+  attr_accessor :mood
 
-  def initialize(name)
+  def initialize(name, mood='nervous')
     @name = name
-    # @mood = mood
+    @mood = mood
   end
 end
